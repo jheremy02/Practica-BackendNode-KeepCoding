0e31aef72f2ab115ca933283ef019855e40c8efe
Jheyson Menéndez <jheremy26@gmail.com>
2022-06-01 21:13:23 -0500

    implemento filtro de busqueda por rango numerico

--- nodepop/models/Advertisement.js
+++ nodepop/models/Advertisement.js
@@ -17,7 +17,29 @@ const advertisementSchema=mongoose.Schema({
 
 advertisementSchema.statics.list=function (filter) {
 
+    let searchByPrice={};
+    
+    if (filter.price) {
+        // rango ejemplo : 50-  de 50 a màs 
+        if (filter.price.indexOf("-")===filter.price.length-1) {
+            filter.price=filter.price.split("-")
+            searchByPrice={ precio: { $gte:filter.price[0] } }
+        } else if (filter.price.indexOf("-")==0) {
+            filter.price=filter.price.split("-")
+            searchByPrice={ precio: { $gte:filter.price[1] } }
+        } else if (filter.price.split("-").length==2) {
+            filter.price=filter.price.split("-")
+            searchByPrice={ price: { $gte: filter.price[0], $lte: filter.price[1] } }
+            
+        } else if (filter.price.indexOf("-")===-1) {
+            searchByPrice={ price: filter.price }
+        } else {
+            searchByPrice={}
+        }
+    }
 
+   
+   
     let searchByTags={};
     if (filter.tags) {
         searchByTags={tags:{$in:filter.tags}}
@@ -29,7 +51,7 @@ advertisementSchema.statics.list=function (filter) {
         searchByType={type_advertisement:filter.type_advertisement}
     }
     
-    const query=Advertisement.find({...searchByTags,...searchByType})
+    const query=Advertisement.find({...searchByTags,...searchByType,...searchByPrice})
     return query.exec().then(data=>{
         data.forEach(row=>{
             row.image=row.image ? path.join('/images/advertisements/',row.image) : null
