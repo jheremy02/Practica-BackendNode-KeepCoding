ecc4e6f5f575d7d3049e71ac445065520485b9cb
Jheyson Menéndez <jheremy26@gmail.com>
2022-06-04 16:42:25 -0500

    limpio el archivo de modelo de anuncio y paso el codigo de validacion hacia la carpeta utils

--- nodepop/models/Advertisement.js
+++ nodepop/models/Advertisement.js
@@ -12,46 +12,23 @@ const advertisementSchema=mongoose.Schema({
     collection:"advertisements" //aqui va el nombre de la collecion
 })
 
+// Tags permitidos
+
+advertisementSchema.statics.allowedTags=function () {
+    return ['work','lifestyle','motor','mobile']
+}
 
 
+advertisementSchema.statics.list=function (filter,pagination) {
 
-advertisementSchema.statics.list=function (filter) {
-
-    let searchByPrice={};
     
-    if (filter.price) {
-        // rango ejemplo : 50-  de 50 a màs 
-        if (filter.price.indexOf("-")===filter.price.length-1) {
-            filter.price=filter.price.split("-")
-            searchByPrice={ precio: { $gte:filter.price[0] } }
-        } else if (filter.price.indexOf("-")==0) {
-            filter.price=filter.price.split("-")
-            searchByPrice={ precio: { $gte:filter.price[1] } }
-        } else if (filter.price.split("-").length==2) {
-            filter.price=filter.price.split("-")
-            searchByPrice={ price: { $gte: filter.price[0], $lte: filter.price[1] } }
-            
-        } else if (filter.price.indexOf("-")===-1) {
-            searchByPrice={ price: filter.price }
-        } else {
-            searchByPrice={}
-        }
-    }
 
-   
-   
-    let searchByTags={};
-    if (filter.tags) {
-        searchByTags={tags:{$in:filter.tags}}
-    }
+    const query=Advertisement.find(filter)
+    query.skip(pagination.start)
+    query.limit(pagination.limit)
+    query.sort(pagination.sort)
 
-    let searchByType={}
 
-    if (filter.type_advertisement) {
-        searchByType={type_advertisement:filter.type_advertisement}
-    }
-    
-    const query=Advertisement.find({...searchByTags,...searchByType,...searchByPrice})
     return query.exec().then(data=>{
         data.forEach(row=>{
             row.image=row.image ? path.join('/images/advertisements/',row.image) : null
