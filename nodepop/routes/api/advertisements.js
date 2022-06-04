a481604a55d5cc8b155d05590973316dbdb9767b
Jheyson Menéndez <jheremy26@gmail.com>
2022-06-04 16:40:14 -0500

    agrego codigo para la ruta de creacion de anuncios

--- nodepop/routes/api/advertisements.js
+++ nodepop/routes/api/advertisements.js
@@ -1,19 +1,31 @@
 const express=require("express")
 const createHttpError = require("http-errors")
 const router=express.Router()
-
+const  {buildFilter}= require('../../lib/utils.js')
 const Advertisement=require("../../models/Advertisement.js")
+const { body, validationResult } = require('express-validator');
 
 
 //GET obtener anuncios
-router.get("/",async (req,res)=>{
+router.get("/",async (req,res,next)=>{
 
-    const filter=req.query
-    console.log(filter)
-    const advertisements= await Advertisement.list(filter)
+    const start=parseInt(req.query.start) || 0
+    const limit = parseInt(req.query.limit) || 100
+    const sort = req.query.sort || '_id'
+    const pagination={start,limit,sort}
+
+    const filters=req.query
+    const filterBuilt=buildFilter(filters)
+    const advertisements= await Advertisement.list(filterBuilt,pagination)
     res.json(advertisements)
 })
 
+router.get('/tags',(req,res)=>{
+    
+    const tags=["work","lifestyle","mobile","motor"]
+    res.json({result:tags})
+
+})
 
 // GET obtener un anuncio
 
@@ -26,10 +38,24 @@ router.get("/:id",(req,res)=>{
 
 //POST : crear anuncios
 
-router.post("/",async (req,res,next)=>{
+router.post("/",[body('tags').custom(tags => {
+    if (!Array.isArray(tags)) {
+        tags=[tags]
+    }
+    const allowed = Advertisement.allowedTags();
+    return tags.every(tag => allowed.includes(tag)) 
+  }).withMessage(`allowed tags ${Advertisement.allowedTags()}`),
+  body('type_advertisement').custom(type_advertisement=>{
+    type_advertisement=[type_advertisement]
+    const allowed = ['sell','buy'];
+    return type_advertisement.every(type=>allowed.includes(type))
+  }).withMessage(`allowed type advertisements: sell , buy`),
+  body('price').isNumeric().withMessage('must be numeric'),
+],async (req,res,next)=>{
 
+    
     try {
-        
+        validationResult(req).throw();
         const advertisementData=req.body;
 
     
@@ -39,8 +65,9 @@ router.post("/",async (req,res,next)=>{
         const advertisementCreated=await advertisement.save()
         res.status(201).json({data:advertisementCreated})
 
-    } catch (error) {
-        next(error)
+    } catch (err) {
+        
+        next(err)
     }
     
 })
@@ -97,6 +124,9 @@ router.delete("/:id",async (req,res,next)=>{
         next(error)
     }
 })
+
+// GET tags 
+
 
 
 module.exports = router
