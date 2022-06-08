d8d8f4af7b5488102996f0392fd3f8bddc442293
Jheyson Menéndez <jheremy26@gmail.com>
2022-06-08 16:27:50 -0500

    agrego vista web de la documentacion

--- nodepop/routes/index.js
+++ nodepop/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+const fs=require('fs')
+const path = require('path');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const filename = path.join(__dirname, '../README.md');
+  fs.readFile(filename,'utf8',(err,readme)=>{
+    console.log(filename)
+    res.render('index', {readme})
+  })
+  ;
 });
 
 module.exports = router;
